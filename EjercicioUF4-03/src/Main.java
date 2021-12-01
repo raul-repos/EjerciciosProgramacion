1c34f22ef91b4b1828a96adc8948d2904a48b57a
Raul Calvo <83084121+Raul-Docs@users.noreply.github.com>
2021-12-01 18:22:01 +0100

    Finished exercise

--- EjercicioUF4-03/src/Main.java
+++ EjercicioUF4-03/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 public class Main {
@@ -17,23 +16,10 @@ public class Main {
         }
 
         lector.close();
-        System.out.println(listado);
-
+        StringTokenizer result = new StringTokenizer(listado.toString(),";"); // Como las ; están metidas ad hoc y no forman parte del texto, NO añado TRUE para incluirlas
+        while (result.hasMoreTokens()){
+            System.out.println(result.nextToken());
+        }
     }
 }
 
-/**
- * Escribe un programa Java que funcione de acuerdo a las siguientes especificaciones:
- *
- * Debe permitir al usuario introducir varios textos terminando cuando escriba la palabra FIN.
- * Cada texto debe guardarse en un objeto de tipo String.
- *
- *
- * Por cada texto introducido (menos la palabra FIN) el programa debe informar del número de caracteres.
- * Se deben ir concatenando todos los textos introducidos convertidos a mayúsculas (menos la palabra FIN)
- * en un objeto de tipo StringBuilder.
- *
- * Dentro del objeto StringBuilder cada texto nuevo debe terminar con el carácter punto y coma.
- * Por último, debemos utilizar un objeto de la clase StringTokenizer para volver a mostrar
- * los textos introducidos en líneas separadas.
- */
